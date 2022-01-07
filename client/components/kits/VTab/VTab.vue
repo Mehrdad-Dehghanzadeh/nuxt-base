02e0d0ee4043b3d9cd58023dbfd3b1240bd0fab6
Mehrdad <mehrdad.dehghanzadeh.dev@gmail.com>
2022-01-07 23:44:06 +0330

    chore: inital tab

---
+++ client/components/kits/VTab/VTab.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="v-tab">
+    <div class="v-tab-bar">
+
+    </div>
+
+    <div class="v-tab__contrainer">
+
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "VTab",
+
+  props: {
+    color: {
+      type: String,
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./VTab.scss" />
