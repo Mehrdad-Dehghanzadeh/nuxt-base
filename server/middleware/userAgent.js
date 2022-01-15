af44c86f3f3149e86722fbd0728a8d205b13d63f
Mehrdad <mehrdad.dehghanzadeh.dev@gmail.com>
2022-01-16 00:14:40 +0330

    feat: add browser agent

--- server/middleware/userAgent.js
+++ server/middleware/userAgent.js
@@ -1,4 +1,10 @@
+const browserInfo = require('browser-info')
+const { checkBrowser } = require('../helpers/utils')
+
 module.exports = (req, res, next) => {
-  // console.log(req.headers['user-agent'])
+  const browser = browserInfo(req.headers['user-agent'])
+  if (!checkBrowser(browser)) {
+    res.redirect('/404')
+  }
   next()
 }
