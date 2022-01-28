944d736a1732ff336842ddd65454266005362d4b
Mehrdad <mehrdad.dehghanzadeh.dev@gmail.com>
2022-01-28 23:27:41 +0330

    chore: inital component for stepper

---
+++ client/components/kits/VStepper/VStepper.vue
@@ -0,0 +1,96 @@
+<template>
+  <section class="v-stepper">
+    <div v-if="nav" class="v-stepper-nav"></div>
+
+    <div :class="['v-stepper__wrapper', { 'direction-ltr': ltr }]">
+      <div class="v-stepper__slider" :style="{ width: sliderWidth }">
+        <slot />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'VStepper',
+
+  props: {
+    value: {
+      type: Number,
+      default: 1
+    },
+    nav: {
+      type: Boolean,
+      default: false
+    },
+    ltr: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data() {
+    return {
+      steps: []
+    }
+  },
+
+  computed: {
+    localValue: {
+      get() {
+        return this.value
+      },
+
+      set(newVal) {
+        this.$nextTick(() => {
+          this.$emit('input', newVal)
+        })
+      }
+    },
+
+    sliderWidth() {
+      const sliderCount = this.steps.length
+      return `${sliderCount > 1 ? sliderCount * 100 : 100}%`
+    }
+  },
+
+  methods: {
+    next() {
+      this.localValue++
+    },
+
+    pervious() {
+      this.localValue--
+    },
+
+    go(step) {
+      this.localValue = step
+    },
+
+    setWidthSteps() {
+      const stepCount = this.steps.length
+      if (stepCount > 1) {
+        this.steps.forEach((element) => {
+          element.width = 100 / stepCount
+        })
+      }
+    }
+  },
+
+  beforeCreate() {
+    this.$nuxt.$on('stepper:next', this.next)
+    this.$nuxt.$on('stepper:pervious', this.pervious)
+    this.$nuxt.$on('stepper:go', this.go)
+  },
+
+  created() {
+    this.steps = this.$children
+  },
+
+  mounted() {
+    this.setWidthSteps()
+  }
+}
+</script>
+
+<style lang="scss" src="./VStepper.scss" />
---
+++ client/components/kits/VStepper/VStepperStep.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="v-stepper-step" :style="{ flex: `0 0 ${width}%` }">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'VStepperStep',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+
+  data() {
+    return {
+      width: 100
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./VStepper.scss" />
