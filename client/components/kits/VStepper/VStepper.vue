90454027bca195ce5c5a606c2e21405b2342ab6b
Mehrdad <mehrdad.dehghanzadeh.dev@gmail.com>
2022-02-01 00:42:15 +0330

    stepper nav

--- client/components/kits/VStepper/VStepper.vue
+++ client/components/kits/VStepper/VStepper.vue
@@ -3,7 +3,10 @@
     <div v-if="nav" class="v-stepper-nav">
       <ul class="v-stepper-nav__wrapper">
         <li v-for="item in navItems" :key="item" class="v-stepper-nav__item">
-          <span>{{ item }}</span>
+          <span class="display-inline-block">
+            <span class="v-stepper-nav__bullet"></span>
+            <p class="v-stepper-nav__title">{{ item }}</p>
+          </span>
         </li>
       </ul>
     </div>
@@ -18,93 +21,93 @@
 
 <script>
 export default {
-  name: 'VStepper',
+  name: "VStepper",
 
   props: {
     value: {
       type: Number,
-      default: 1
+      default: 1,
     },
     nav: {
       type: Boolean,
-      default: false
+      default: false,
     },
     ltr: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
 
   data() {
     return {
       steps: [],
-      navItems: []
-    }
+      navItems: [],
+    };
   },
 
   computed: {
     localValue: {
       get() {
-        return this.value
+        return this.value;
       },
 
       set(newVal) {
         this.$nextTick(() => {
-          this.$emit('input', newVal)
-        })
-      }
+          this.$emit("input", newVal);
+        });
+      },
     },
 
     sliderWidth() {
-      const sliderCount = this.steps.length
-      return `${sliderCount > 1 ? sliderCount * 100 : 100}%`
-    }
+      const sliderCount = this.steps.length;
+      return `${sliderCount > 1 ? sliderCount * 100 : 100}%`;
+    },
   },
 
   methods: {
     next() {
-      this.localValue++
+      this.localValue++;
     },
 
     pervious() {
-      this.localValue--
+      this.localValue--;
     },
 
     go(step) {
-      this.localValue = step
+      this.localValue = step;
     },
 
     setWidthSteps() {
-      const stepCount = this.steps.length
+      const stepCount = this.steps.length;
       if (stepCount > 1) {
         this.steps.forEach((element) => {
-          element.width = 100 / stepCount
-        })
+          element.width = 100 / stepCount;
+        });
       }
     },
 
     setNavItems() {
       this.steps.forEach((el) => {
-        this.navItems.push(el.title)
-      })
-    }
+        this.navItems.push(el.title);
+      });
+    },
   },
 
   beforeCreate() {
-    this.$nuxt.$on('stepper:next', this.next)
-    this.$nuxt.$on('stepper:pervious', this.pervious)
-    this.$nuxt.$on('stepper:go', this.go)
+    this.$nuxt.$on("stepper:next", this.next);
+    this.$nuxt.$on("stepper:pervious", this.pervious);
+    this.$nuxt.$on("stepper:go", this.go);
   },
 
   created() {
-    this.steps = this.$children
+    this.steps = this.$children;
   },
 
   mounted() {
-    this.setWidthSteps()
-    this.setNavItems()
-  }
-}
+    this.setWidthSteps();
+    this.setNavItems();
+  },
+};
 </script>
 
 <style lang="scss" src="./VStepper.scss" />
