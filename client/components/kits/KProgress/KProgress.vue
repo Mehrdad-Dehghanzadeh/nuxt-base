f708d6e4881787151a8e515987866bc486ee7f6b
Mehrdad Dehghanzadeh <77623680+Mehrdad-Dehghanzadeh@users.noreply.github.com>
2022-05-20 20:29:25 +0430

    feat: add KProgress

---
+++ client/components/kits/KProgress/KProgress.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="['k-progress', { 'k-progress--determinate': determinate }]">
+    <div v-if="determinate" class="k-progress__line">
+      <span class="k-progress__value" />
+      <span class="k-progress__value" />
+    </div>
+
+    <div v-else class="k-progress__line">
+      <span class="k-progress__value" :style="{ width: `${value}%` }" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KProgress',
+
+  props: {
+    value: {
+      type: [String, Number],
+      default: 0
+    },
+    determinate: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style src="./KProgress.scss" lang="scss" />
