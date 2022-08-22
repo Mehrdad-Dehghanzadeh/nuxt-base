a363a74973d1d10b0dd2ed9c938a4e8b07682b85
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2022-08-22 17:45:07 +0430

    feat: add time and day validation

--- client/assets/helpers/validate.js
+++ client/assets/helpers/validate.js
@@ -92,7 +92,46 @@ export const checkMobile = function (value) {
 /**
  * check zero value
  * ***********************************/
-
 export const noZero = (value) => {
   return Number(value) != 0
+}
+
+/**
+ * check date format  yyyy-mm-dd
+ * ***********************************/
+export function date(val) {
+  // First check for the pattern
+  var regex_date = /^\d{4}\-\d{2}\-\d{2}$/
+
+  if (!regex_date.test(val)) {
+    return false
+  }
+
+  // Parse the date parts to integers
+  var parts = val.split('-')
+  var day = parseInt(parts[2], 10)
+  var month = parseInt(parts[1], 10)
+  var year = parseInt(parts[0], 10)
+
+  // Check the ranges of month and year
+  if (year < 1000 || year > 3000 || month == 0 || month > 12) {
+    return false
+  }
+
+  var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+  // Adjust for leap years
+  if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+    monthLength[1] = 29
+  }
+
+  // Check the range of the day
+  return day > 0 && day <= monthLength[month - 1]
+}
+
+/**
+ * check time format  hh:mm:ss or hh:mm
+ * ***********************************/
+export function time(val) {
+  return /^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$/.test(val)
 }
